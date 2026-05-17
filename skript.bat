af66981dae7d7161700e99dc7e8cbbf4532de0f1
zeeez-888 <alexchen417@gmail.com>
2026-05-18 01:57:44 +0300

    Add files via upload

---
+++ skript.bat
@@ -0,0 +1,39 @@
+echo off
+rem create new catalog
+md Chen
+rem go to into new catalog
+cd Chen
+md Alexey
+cd Alexey
+md Ir-Manovich
+rem this need for pause of programm
+cd..
+cd..
+pause
+echo off
+rem go to into Chen
+cd Chen
+rem create bithday
+echo > 28112005.txt
+cd Alexey
+cd Ir-Manovich
+rem create num
+echo > xiaomi.txt
+cd..
+cd..
+cd..
+pause
+echo off
+del Ir-Manovich /S /Q /F
+del Alexey /S /Q /F
+del Chen /S /Q /F
+pause
+echo off
+cd Chen
+cd Alexey
+rd Ir-Manovich
+cd..
+rd Alexey
+cd ..
+rd Chen
+pause
